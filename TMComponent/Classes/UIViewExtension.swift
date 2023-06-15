b31434bfe5952d6ba2fd6ea4bb4d58c1c74fe8ac
Jason Zhang <1573989249@qq.com>
2023-06-15 13:28:42 +0800

    public UIViewExtension

--- TMComponent/Classes/UIViewExtension.swift
+++ TMComponent/Classes/UIViewExtension.swift
@@ -9,34 +9,34 @@ import Foundation
 import UIKit
 
 extension UIView {
-    func setShadow(_ shadowRadius: CGFloat, _ shadowOffset: CGSize, _ shadowColor: CGColor = UIColor.black.cgColor, _ shadowOpacity: Float = 0.5) {
+    public func setShadow(_ shadowRadius: CGFloat, _ shadowOffset: CGSize, _ shadowColor: CGColor = UIColor.black.cgColor, _ shadowOpacity: Float = 0.5) {
         layer.shadowRadius = shadowRadius
         layer.shadowOffset = shadowOffset
         layer.shadowColor = shadowColor
         layer.shadowOpacity = shadowOpacity
     }
 
-    func setCorner(radii: CGFloat, masksToBounds: Bool = true) {
+    public func setCorner(radii: CGFloat, masksToBounds: Bool = true) {
         layer.cornerRadius = radii
         layer.masksToBounds = masksToBounds
     }
 
-    func drawBorder(color: UIColor, width: CGFloat) {
+    public func drawBorder(color: UIColor, width: CGFloat) {
         layer.borderColor = color.cgColor
         layer.borderWidth = width
     }
 
-    func addTapGesture(_ target: Any, _ action: Selector?) {
+    public func addTapGesture(_ target: Any, _ action: Selector?) {
         let tap = UITapGestureRecognizer(target: target, action: action)
         addGestureRecognizer(tap)
     }
 
-    func removeTapGesture(_ target: Any, _ action: Selector?) {
+    public func removeTapGesture(_ target: Any, _ action: Selector?) {
         let tap = UITapGestureRecognizer(target: target, action: action)
         removeGestureRecognizer(tap)
     }
 
-    func addAnimation(_ fromValue: Any?, _ toValue: Any?, _ duration: CFTimeInterval, _ forKey: String?) {
+    public func addAnimation(_ fromValue: Any?, _ toValue: Any?, _ duration: CFTimeInterval, _ forKey: String?) {
         let animation = CABasicAnimation()
         animation.duration = duration
         animation.fromValue = fromValue
@@ -46,7 +46,7 @@ extension UIView {
         layer.add(animation, forKey: forKey)
     }
 
-    func addAnimation(_ fromValue: Any?, _ toValue: Any?, _ duration: CFTimeInterval, _ forKey: String?, completionHandler: @escaping () -> Void) {
+    public func addAnimation(_ fromValue: Any?, _ toValue: Any?, _ duration: CFTimeInterval, _ forKey: String?, completionHandler: @escaping () -> Void) {
         let animation = CABasicAnimation()
         animation.duration = duration
         animation.fromValue = fromValue
